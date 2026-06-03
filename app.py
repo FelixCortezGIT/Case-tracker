a7fb8710ff069cca210b635609b01f5ab792798f
TomasMacula <tomasmacula@live.com>
2026-06-03 13:57:20 +0200

    pip freeze > requirements.txt + gunicorn

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class Case(db.Model):
     status = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     deadline_date = db.Column(db.Date)
-
+ 
 @app.route("/")
 def home():
     return "home page"
